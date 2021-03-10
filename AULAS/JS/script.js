1998611ca9661bd3590c4c6ebf208ff6e0d419ed
Marevandro <marevandro95@gmail.com>
2021-03-09 22:32:42 -0300

    Primeiros passos com JS.
    variável e função dentro do JavaScript.

---
+++ AULAS/JS/script.js
@@ -0,0 +1,13 @@
+// alert("Olá, mundo!");
+/*var resposta = confirm("Como esta? ")
+console.log(resposta);*/
+
+var a = 10;
+var b = 20;
+alert (a + b);
+
+function somar (num1, num2){
+
+    return num1 + num2;
+}
+console.log(somar (3, 12));
