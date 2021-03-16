e9f4ec7b1855257eab28ca496ee27281dfc26f3d
Marevandro <marevandro95@gmail.com>
2021-03-15 22:48:19 -0300

    Funções no JS
    Verificando as funções em JS e criando um botão no HTML.

--- AULAS/JS/script.js
+++ AULAS/JS/script.js
@@ -1,8 +1,8 @@
 // alert("Olá, mundo!");
 /*var resposta = confirm("Como esta? ")
 console.log(resposta);*/
-
-var a = 10;
+//aula 09
+/*var a = 10;
 var b = 20;
 alert (a + b);
 
@@ -11,3 +11,25 @@ function somar (num1, num2){
     return num1 + num2;
 }
 console.log(somar (3, 12));
+*/
+
+//aula #10
+// Propriedades são elementos que eu possa mudar
+//Metodos são ações que eu possa executar
+
+const a = 10;
+const b = 33;
+function somar (a, b){
+    return parseInt(a) + parseInt(b);
+}
+
+document.querySelector("#calcular").addEventListener("click", function () {
+    let valorA = document.querySelector("#valorA").value;
+    let valorB = document.querySelector("#valorB").value;
+    if(valorA.length > 0 && valorB.length > 0 ){
+        alert (parseInt(valorA) + parseInt(valorB));
+        //alert somar(valorA, valorB);
+    }else{
+        alert ("Digite os valores para cálculo");   
+    }
+} );
